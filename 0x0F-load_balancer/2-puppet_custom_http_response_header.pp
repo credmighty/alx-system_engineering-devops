dada2e3fcae28a8c7f6e2cb16b738b22c9d256d8
Abdullahi Abdulrofiu <abdulrofiuabdullahi@gmail.com>
2024-04-30 01:17:09 +0100

    task 2 -pplint

--- 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
+++ 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
@@ -2,6 +2,7 @@
 #Install nginx and add http header
 
 exec {'Install nginx':
-  command  => 'sudo apt-get -y update && sudo apt-get -y install nginx && sudo sed -i "15i add_header X-Served-By \$hostname;" /etc/nginx/nginx.conf && sudo service nginx restart',
-    provider => shell,
-    }
+  command  => 'sudo apt-get -y update && sudo apt-get -y install nginx && sudo sed -i "15i add_header X-Served-By \$hostname;"
+	/etc/nginx/nginx.conf && sudo service nginx restart',
+  provider => shell,
+}
