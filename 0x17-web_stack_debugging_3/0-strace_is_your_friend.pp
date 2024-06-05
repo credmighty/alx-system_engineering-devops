a0b5b61f1db4577a00c72369fe4ccdbe7c025ea6
Abdullahi Abdulrofiu <abdulrofiuabdullahi@gmail.com>
2024-06-05 18:29:44 +0100

    debugging #3

---
+++ 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -0,0 +1,7 @@
+# Fixes Apache 500 error
+
+exec { 'fix-wordpress':
+  command => 'sed -i s/phpp/php/g /var/www/html/wp-settings.php',
+  path    => '/usr/local/bin/:/bin/'
+  onlyif  => 'test -f /var/www/html/wp-settings.php'
+}
