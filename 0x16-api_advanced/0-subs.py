33d75681e4431c8b5794864541d302dc2b3ef3ec
Abdullahi Abdulrofiu <abdulrofiuabdullahi@gmail.com>
2024-06-05 01:01:47 +0100

    api advance

---
+++ 0x16-api_advanced/0-subs.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+""""""
+
+import requests
+
+def number_of_subscribers(subreddit):
+    """reddit api that returns no of sub"""
+    url = 'http://www.reddit.com/r/{}/about.json'.format(sunreddit)
+    headers = {'User-Agent': 'My User Agent 1.0'}
+    response = requests.get(url, headers=headers)
+    if response.status_code == 200:
+        data = reponse.json()
+        return data.get('data').get('subscribers')
+    else:
+        return 0
